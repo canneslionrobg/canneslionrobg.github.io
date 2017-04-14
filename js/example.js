1dec64ab0acafb1b63943898f942e1f6595b1b49
Rob G <rob.griffiths@icrossing.co.uk>
2017-04-14 22:19:16 +0100

    update radial heat chart

--- js/example.js
+++ js/example.js
@@ -16,7 +16,7 @@ var chart = circularHeatChart()
 // }
 
 d3.json("seasonaldata.json", function(data) {
-  var chartData = data;
+  Console.log(data);
 });
 
 chart.accessor(function(d) {return d.value;})
