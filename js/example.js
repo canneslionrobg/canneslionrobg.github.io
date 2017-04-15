1b67bd967a7d57171ca08086ed69858962cb09b9
Rob G <rob.griffiths@icrossing.co.uk>
2017-04-15 22:16:16 +0100

    update circular heat map

--- js/example.js
+++ js/example.js
@@ -10,17 +10,17 @@ var chart = circularHeatChart()
 
 
 /* An array of objects */
-// data = [];
-// for(var i=0; i<48; i++) {
-//     data[i] = {title: "Segment "+i, value: Math.round(Math.random()*100)};
-// }
+data = [];
+for(var i=0; i<48; i++) {
+    data[i] = {title: "Segment "+i, value: Math.round(Math.random()*100)};
+}
 
-var data; // a global
-d3.json("seasonaldata.json", function(error, json) {
-  if (error) return console.warn(error);
-  data = json;
-  console.log(data[0]);
-});
+// var data; // a global
+// d3.json("seasonaldata.json", function(error, json) {
+//   if (error) return console.warn(error);
+//   data = json;
+//   console.log(data[0]);
+// });
 
 chart.accessor(function(d) {return d.value;})
     .radialLabels(null)
